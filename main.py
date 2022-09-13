f1c5cfb2aed14147fe9232ef13404b87fe9f3fc5
unknown <odvnagual@gmail.com>
2022-09-14 01:48:53 +0400

    added archive options, working on proper archiving

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import os
 
 
 def unzip(filepath):
-    if os.path.exists('Sample') == False or os.path.exists('Sample/temp') == False:
+    if os.path.exists('Sample') is False or os.path.exists('Sample/temp') is False:
         os.mkdir('Sample')
         os.mkdir('Sample/temp')
     with zipfile.ZipFile(filepath, 'r') as target:
         target.extractall('Sample/temp')
 
-def rezip():
-    for root, dirs, files in os.walk('Sample/temp'):
 
-unzip('Sample/sample1.docx')
+def rezip(path, target):
+    rootdir = os.path.basename(path)
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            parentpath = os.path.relpath(filepath, path)
+            arcname = os.path.join(rootdir, parentpath)
+            target.write(filepath, arcname)
+
+
+with zipfile.ZipFile('Target.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+    rezip('Sample/temp', zipf)
